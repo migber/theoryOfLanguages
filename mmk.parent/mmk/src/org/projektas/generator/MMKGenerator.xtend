14049046248510cf7c130d7a297ff300f1a2cbf4
mariusblazys <marius.blazys16@gmail.com>
2017-05-22 19:45:45 +0300

    generator

--- mmk.parent/mmk/src/org/projektas/generator/MMKGenerator.xtend
+++ mmk.parent/mmk/src/org/projektas/generator/MMKGenerator.xtend
@@ -5,21 +5,23 @@ package org.projektas.generator
 
 import org.eclipse.emf.ecore.resource.Resource
 import org.eclipse.xtext.generator.AbstractGenerator
-import org.eclipse.xtext.generator.IFileSystemAccess2
+import org.eclipse.xtext.generator.IFileSystemAccess
 import org.eclipse.xtext.generator.IGeneratorContext
+import org.projektas.mMK.Model
+import org.eclipse.xtext.generator.IGenerator
 
 /**
  * Generates code from your model files on save.
  * 
  * See https://www.eclipse.org/Xtext/documentation/303_runtime_concepts.html#code-generation
  */
-class MMKGenerator extends AbstractGenerator {
+class MMKGenerator implements IGenerator {
 
-	override void doGenerate(Resource resource, IFileSystemAccess2 fsa, IGeneratorContext context) {
-//		fsa.generateFile('greetings.txt', 'People to greet: ' + 
-//			resource.allContents
-//				.filter(Greeting)
-//				.map[name]
-//				.join(', '))
+	override void doGenerate(Resource resource, IFileSystemAccess fsa) {
+		        fsa.generateFile("relative/path/AllTheStates.txt", '''
+            «FOR state : resource.allContents.filter(Model).toIterable»
+                Model «Model.name»
+            «ENDFOR»
+        ''')
 	}
 }
--- mmk.parent/mmk/xtend-gen/org/projektas/generator/MMKGenerator.java
+++ mmk.parent/mmk/xtend-gen/org/projektas/generator/MMKGenerator.java
@@ -3,10 +3,13 @@
  */
 package org.projektas.generator;
 
+import com.google.common.collect.Iterators;
 import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.xtext.generator.AbstractGenerator;
-import org.eclipse.xtext.generator.IFileSystemAccess2;
-import org.eclipse.xtext.generator.IGeneratorContext;
+import org.eclipse.xtend2.lib.StringConcatenation;
+import org.eclipse.xtext.generator.IFileSystemAccess;
+import org.eclipse.xtext.generator.IGenerator;
+import org.eclipse.xtext.xbase.lib.IteratorExtensions;
+import org.projektas.mMK.Model;
 
 /**
  * Generates code from your model files on save.
@@ -14,8 +17,19 @@ import org.eclipse.xtext.generator.IGeneratorContext;
  * See https://www.eclipse.org/Xtext/documentation/303_runtime_concepts.html#code-generation
  */
 @SuppressWarnings("all")
-public class MMKGenerator extends AbstractGenerator {
+public class MMKGenerator implements IGenerator {
   @Override
-  public void doGenerate(final Resource resource, final IFileSystemAccess2 fsa, final IGeneratorContext context) {
+  public void doGenerate(final Resource resource, final IFileSystemAccess fsa) {
+    StringConcatenation _builder = new StringConcatenation();
+    {
+      Iterable<Model> _iterable = IteratorExtensions.<Model>toIterable(Iterators.<Model>filter(resource.getAllContents(), Model.class));
+      for(final Model state : _iterable) {
+        _builder.append("Model ");
+        String _name = Model.class.getName();
+        _builder.append(_name);
+        _builder.newLineIfNotEmpty();
+      }
+    }
+    fsa.generateFile("relative/path/AllTheStates.txt", _builder);
   }
 }
